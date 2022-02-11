bd478541ffa5312ade39e405eab6a350991a50e1
franco <x.francoruiz@gmail.com>
2022-02-11 02:33:56 -0300

    delete content

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-import { ApolloServer, gql } from "apollo-server";
-
 const persons = [
   {
     name: "Frank",
@@ -29,34 +27,3 @@ const persons = [
     id: "28746490189373",
   },
 ];
-
-const typeDefs = gql`
-  type Person {
-    name: String!
-    phone: String
-    street: String!
-    city: String!
-    id: ID!
-  }
-
-  type Query {
-    personCount: Int!
-    allPersons: [Person]!
-  }
-`;
-
-const resolvers = {
-  Query: {
-    personCount: () => persons.length,
-    allPersons: () => persons,
-  },
-};
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
-
-server.listen().then(({ url }) => {
-  console.log(`Server ready at ${url}`);
-});
